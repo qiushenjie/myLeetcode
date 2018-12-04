94343cc23632e45e8141fe9d58560578abc344ad
qiushenjie <35244546+qiushenjie@users.noreply.github.com>
2018-12-04 11:11:33 +0800

    Add files via upload

---
+++ 98_验证二叉搜索树.py
@@ -0,0 +1,59 @@
+# 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+# 假设一个二叉搜索树具有如下特征：
+
+# 节点的左子树只包含小于当前节点的数。
+# 节点的右子树只包含大于当前节点的数。
+# 所有左子树和右子树自身必须也是二叉搜索树。
+# 示例 1:
+
+# 输入:
+#     2
+#    / \
+#   1   3
+# 输出: true
+# 示例 2:
+
+# 输入:
+#     5
+#    / \
+#   1   4
+#      / \
+#     3   6
+# 输出: false
+# 解释: 输入为: [5,1,4,null,null,3,6]。
+#      根节点的值为 5 ，但是其右子节点值为 4
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isValidBST(self, root):
+    #     if self.helper(root, root.val):
+    #         return True
+    #     else:
+    #         return False
+
+    # def helper(self, root, parentval):
+    #     if root:
+    #         if root.left and root.right:
+    #             if root.left.val < parentval and self.helper(root.left, root.left.val) and root.right.val > parentval and self.helper(root.right, root.right.val):
+    #                 return True
+    #             else:
+    #                 return False
+    #         elif root.left:
+    #             if root.left.val < parentval and self.helper(root.left, root.left.val):
+    #                 return True
+    #             else:
+    #                 return False
+    #         elif root.right:
+    #             if root.right.val > parentval and self.helper(root.right, root.right.val):
+    #                 return True
+    #             else:
+    #                 return False
+    #         else:
+    #             return True
+    #     else:
+    #         return True
