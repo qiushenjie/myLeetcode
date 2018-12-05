1ebb1ec4358ac10342a56cb430c43febd3424b60
qiushenjie <35244546+qiushenjie@users.noreply.github.com>
2018-12-05 11:10:37 +0800

    Add files via upload

---
+++ 107._二叉树的层次遍历II.py
@@ -0,0 +1,36 @@
+# 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+# 例如：
+# 给定二叉树 [3,9,20,null,null,15,7],
+
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# 返回其自底向上的层次遍历为：
+
+# [
+#   [15,7],
+#   [9,20],
+#   [3]
+# ]
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root):
+        if not root:
+            return []
+        res = []
+        level = [root]
+        while level:
+            res.append([node.val for node in level])
+            temp = []
+            for node in level:
+                temp.extend([node.left, node.right])
+            level = [leafNode for leafNode in temp if leafNode]
+        return res[::-1]
